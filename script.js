41e46699cc65ee5a03ce05d8e846754e3da8b2be
Ashton Sawyer <ashtonesawyer@gmail.com>
2023-05-18 11:12:13 -0700

    add favicon, setup navbar, mock about page content

---
+++ script.js
@@ -0,0 +1,44 @@
+// set up page header
+let head = document.querySelector("#head");
+
+// -- create elements
+let nav = document.createElement("nav");
+let list = document.createElement("ul");
+let listItems = [
+  document.createElement("li"),
+  document.createElement("li"),
+  document.createElement("li"),
+  document.createElement("li"),
+];
+let links = [
+  document.createElement("a"),
+  document.createElement("a"),
+  document.createElement("a"),
+  document.createElement("a"),
+];
+
+// -- add attributes
+let linkText = ["About", "Previous Work", "Projects", "Contact"];
+let pageLinks = [
+  "./index.html",
+  "./previous-work.html",
+  "./projects.html",
+  "./contact.html",
+];
+
+nav.setAttribute("class", "menu");
+list.setAttribute("class", "menu-list");
+for (let i = 0; i < 4; i++) {
+  listItems[i].setAttribute("class", "menu-li");
+  links[i].setAttribute("class", "menu-tab");
+  links[i].setAttribute("href", pageLinks[i]);
+  links[i].textContent = linkText[i];
+}
+
+// -- append elements
+for (let i = 0; i < 4; i++) {
+  listItems[i].append(links[i]); //link to list item
+  list.append(listItems[i]); // list item to list
+}
+nav.append(list);
+head.append(nav);
