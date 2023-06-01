430e8a98d56e922442811599b34a6a88a6db3a61
Ashton Sawyer <ashtonesawyer@gmail.com>
2023-06-01 11:19:35 -0700

    fixed carousel, gen styling

--- carousel.js
+++ carousel.js
@@ -1,15 +1,13 @@
 let carousel = document.querySelector(".carousel");
 let showing = document.querySelector(".showing");
 let cards = document.querySelectorAll(".card");
-let prev = document.querySelector("#prev");
 let arrows = document.querySelectorAll(".arrow");
 
 let index = 1; // start on first card
 const slide = () => {
   // Calculate the updated image index
-  // if (index === cards.length) index = 0;
-  // else if (index < 0) index = cards.length - 1;
-  index = index === cards.length ? 0 : index < 0 ? cards.length - 1 : index;
+  if (index === cards.length) index = 0;
+  else if (index < 0) index = cards.length - 1;
   // Update the carousel display to show the specified image
   showing.style.transform = `translate(-${index * 500}px)`;
 };
